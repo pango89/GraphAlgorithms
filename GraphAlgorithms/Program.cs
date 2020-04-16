fd6b3797a3b4e1634b2cad094ef6ead433d54c68
Pankaj Goyal <pankaj.goyal@optym.com>
2020-04-16 17:42:29 +0530

    Add project files.

---
+++ GraphAlgorithms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GraphAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            UndirectedGraph graph = new UndirectedGraph(4);
+
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 3);
+
+            graph.DFS(2);
+        }
+    }
+}
---
+++ GraphAlgorithms/UndirectedGraph.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphAlgorithms
+{
+    class UndirectedGraph
+    {
+        private readonly int vertices;
+        private readonly List<int>[] adjacencyList;
+
+        public UndirectedGraph(int vertices)
+        {
+            this.vertices = vertices;
+            this.adjacencyList = new List<int>[vertices];
+
+            for (int i = 0; i < this.vertices; i++)
+                this.adjacencyList[i] = new List<int>();
+        }
+
+        public void AddEdge(int u, int v)
+        {
+            this.adjacencyList[u].Add(v);
+            this.adjacencyList[v].Add(u);
+        }
+
+        public void DFS(int i)
+        {
+            bool[] visited = new bool[this.vertices];
+            DFSUtil(i, visited);
+        }
+
+        private void DFSUtil(int i, bool[] visited)
+        {
+            if (visited[i])
+                return;
+
+            Console.Write(i + " ");
+
+            visited[i] = true;
+
+            for (int j = 0; j < this.adjacencyList[i].Count; j++)
+            {
+                int next = this.adjacencyList[i][j];
+
+                if (!visited[next])
+                    DFSUtil(next, visited);
+            }
+        }
+    }
+}
