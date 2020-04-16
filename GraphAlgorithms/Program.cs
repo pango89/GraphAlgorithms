b4ad35a51545ef02c077dc4a944eda6b766705b5
Pankaj Goyal <pankaj.goyal@optym.com>
2020-04-16 19:20:17 +0530

    Added BFS

--- GraphAlgorithms/Program.cs
+++ GraphAlgorithms/Program.cs
@@ -7,16 +7,16 @@ namespace GraphAlgorithms
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            UndirectedGraph graph = new UndirectedGraph(4);
+            UndirectedGraph graph = new UndirectedGraph(6);
 
-            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 1);  // This will add (1, 0) also.
             graph.AddEdge(0, 2);
             graph.AddEdge(1, 2);
-            graph.AddEdge(2, 0);
             graph.AddEdge(2, 3);
-            graph.AddEdge(3, 3);
+            graph.AddEdge(4, 5);
 
-            graph.DFS(2);
+            graph.DFS();
+            graph.BFS();
         }
     }
 }
--- GraphAlgorithms/UndirectedGraph.cs
+++ GraphAlgorithms/UndirectedGraph.cs
@@ -24,6 +24,9 @@ namespace GraphAlgorithms
             this.adjacencyList[v].Add(u);
         }
 
+        // DFS for all vertex one by one
+        // Following implementation does the complete graph 
+        // traversal even if the nodes are unreachable.
         public void DFS()
         {
             bool[] visited = new bool[this.vertices];
@@ -31,10 +34,14 @@ namespace GraphAlgorithms
             for (int i = 0; i < this.vertices; i++)
             {
                 if (!visited[i])
+                {
                     DFSUtil(i, visited);
+                    Console.WriteLine();
+                }
             }
         }
 
+        // DFS only for a given vertex
         public void DFS(int i)
         {
             bool[] visited = new bool[this.vertices];
@@ -56,6 +63,46 @@ namespace GraphAlgorithms
 
                 if (!visited[next])
                     DFSUtil(next, visited);
+            }
+        }
+
+        public void BFS()
+        {
+            bool[] visited = new bool[this.vertices];
+
+            for (int i = 0; i < this.vertices; i++)
+            {
+                if (!visited[i])
+                {
+                    BFSUtil(i, visited);
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private void BFSUtil(int i, bool[] visited)
+        {
+            if (visited[i])
+                return;
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(i);
+
+            while (queue.Count > 0)
+            {
+                int item = queue.Dequeue();
+                Console.Write(item + " ");
+                visited[item] = true;
+
+                for (int j = 0; j < this.adjacencyList[item].Count; j++)
+                {
+                    int next = this.adjacencyList[item][j];
+                    if (!visited[next])
+                    {
+                        queue.Enqueue(next);
+                        visited[next] = true;
+                    }
+                }
             }
         }
     }
